e7fa6bc97359b4122efa2a10eedfb774ec95dc58
Jovane <jovanecano1995@gmail.com>
2024-01-13 19:41:03 -0800

    "working on setting up flask"
    Signed-off-by: Jovane <jovanecano1995@gmail.com>

--- Module_10_C hallenge/SurfsUp/app.py
+++ Module_10_C hallenge/SurfsUp/app.py
@@ -10,25 +10,24 @@ import numpy as np
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine('sqlite:///Resources/hawaii.sqlite').connect
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
